1121f74d1711ee71d4bb3db711bb9ad23655b65c
ShuttleJosh <45016627+ShuttleJosh@users.noreply.github.com>
2025-05-01 20:53:43 +0000

    Added Results

--- quiz_data.py
+++ quiz_data.py
@@ -2,86 +2,85 @@ quiz = [
     {
         "question": "Pick the event you most relate to",
         "options": [
-            {"image": "images/New_Year.JPG", "tag": "Wine"},
-            {"image": "images/Christmas.JPG", "tag": "Wine"},
-            {"image": "images/Halloween.JPG", "tag": "Wine"}
+            {"image": "images/New_Year.JPG", "tag": "Happy"},
+            {"image": "images/Christmas.JPG", "tag": "Adventure"},
+            {"image": "images/Halloween.JPG", "tag": "Crazy"}
         ]
     },
     {
         "question": "Pick the food you're dying for",
         "options": [
-            {"image": "images/Breakfast.jpg", "tag": "Wine"},
-            {"image": "images/Lunch_Buffet.JPG", "tag": "Wine"},
-            {"image": "images/Dinner_Buffet.JPG", "tag": "Wine"},
-            {"image": "images/Fancy_Food.JPG", "tag": "Wine"}
+            {"image": "images/Breakfast.jpg", "tag": "Crazy"},
+            {"image": "images/Lunch_Buffet.JPG", "tag": "Adventure"},
+            {"image": "images/Dinner_Buffet.JPG", "tag": "Crazy"},
+            {"image": "images/Fancy_Food.JPG", "tag": "Happy"}
         ]
     },
     {
         "question": "Thirsty? What drink tempts you",
         "options": [
-            {"image": "images/Wine.JPG", "tag": "Wine"},
-            {"image": "images/Cocktail.JPEG", "tag": "Wine"},
-            {"image": "images/Beer.JPG", "tag": "Wine"},
-            {"image": "images/Coconut.JPEG", "tag": "Wine"},
-            {"image": "images/Coffee.JPEG", "tag": "Wine"},
-            {"image": "images/Iced Coffee.jpg", "tag": "Wine"}
+            {"image": "images/Wine.JPG", "tag": "Happy"},
+            {"image": "images/Cocktail.JPEG", "tag": "Crazy"},
+            {"image": "images/Beer.JPG", "tag": "Happy"},
+            {"image": "images/Coconut.JPEG", "tag": "Crazy"},
+            {"image": "images/Coffee.JPEG", "tag": "Adventure"},
+            {"image": "images/Iced Coffee.jpg", "tag": "Adventure"}
         ]
     },
     {
         "question": "Time to relax, wyd?",
         "options": [
-            {"image": "images/HotTub.JPG", "tag": "Wine"},
-            {"image": "images/Hiking.JPG", "tag": "Wine"},
-            {"image": "images/Exploring.JPG", "tag": "Wine"},
-            {"image": "images/Festival.JPEG", "tag": "Wine"}
+            {"image": "images/HotTub.JPG", "tag": "Adventure"},
+            {"image": "images/Hiking.JPG", "tag": "Happy"},
+            {"image": "images/Exploring.JPG", "tag": "Crazy"},
+            {"image": "images/Festival.JPEG", "tag": "Crazy"}
         ]
     },
     {
         "question": "Let's flyyyyy away... where?",
         "options": [
-            {"image": "images/Brussels.JPEG", "tag": "Wine"},
-            {"image": "images/Budapest.JPEG", "tag": "Wine"},
-            {"image": "images/Corfu.JPEG", "tag": "Wine"},
-            {"image": "images/Venice.JPEG", "tag": "Wine"}
+            {"image": "images/Brussels.JPEG", "tag": "Adventure"},
+            {"image": "images/Budapest.JPEG", "tag": "Happy"},
+            {"image": "images/Corfu.JPEG", "tag": "Crazy"},
+            {"image": "images/Venice.JPEG", "tag": "Happy"}
         ]
     },
     {
         "question": "NIGHTMARE NIGHTMARE NIGHTMARE",
         "options": [
-            {"image": "images/Demon1.JPG", "tag": "Wine"},
-            {"image": "images/Demon2.JPG", "tag": "Wine"},
-            {"image": "images/Demon3.JPG", "tag": "Wine"}
+            {"image": "images/Demon1.JPG", "tag": "Adventure"},
+            {"image": "images/Demon2.JPG", "tag": "Crazy"},
+            {"image": "images/Demon3.JPG", "tag": "Happy"}
         ]
     },
     {
         "question": "You just got fired ): What careers next?",
         "options": [
-            {"image": "images/Racing.JPG", "tag": "Wine"},
-            {"image": "images/Basketball.JPEG", "tag": "Wine"},
-            {"image": "images/Fight.JPEG", "tag": "Wine"}
+            {"image": "images/Racing.JPG", "tag": "Crazy"},
+            {"image": "images/Basketball.JPEG", "tag": "Adventure"},
+            {"image": "images/Fight.JPEG", "tag": "Happy"}
         ]
     },
     {
         "question": "Meme review",
         "options": [
-            {"image": "images/Meme1.JPEG", "tag": "Wine"},
-            {"image": "images/Meme2.JPEG", "tag": "Wine"},
-            {"image": "images/Meme3.JPG", "tag": "Wine"}
+            {"image": "images/Meme1.JPEG", "tag": "Adventure"},
+            {"image": "images/Meme2.JPEG", "tag": "Happy"},
+            {"image": "images/Meme3.JPG", "tag": "Crazy"}
         ]
     },
     {
         "question": "Sleep ",
         "options": [
-            {"image": "images/Sleep1.JPEG", "tag": "Wine"},
-            {"image": "images/Sleep2.JPEG", "tag": "Wine"},
-            {"image": "images/Sleep3.JPG", "tag": "Wine"}
+            {"image": "images/Sleep1.JPEG", "tag": "Happy"},
+            {"image": "images/Sleep2.JPEG", "tag": "Crazy"},
+            {"image": "images/Sleep3.JPG", "tag": "Adventure"}
         ]
     }
 ]
 
 results = {
-    "Wine": "You're Vacation Us ☀️",
-    "cozy": "You're Cozy Movie Night Us 🍿",
-    "romantic": "You're Romantic Dinner Us 💕",
-    "adventure": "You're Hiking Us 🏞️"
+    "Happy": "You got happy Ece! You are a ray of sunshine ☀️",
+    "Crazy": "You got crazy Ece! You are gremlin 🍿",
+    "Adventure": "You got Adventure Ece! Just another day in the life 💕",
 }
