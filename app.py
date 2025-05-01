a3d42abc4472f46cb1b59f46ed0bf4e50bfdedc2
ShuttleJosh <45016627+ShuttleJosh@users.noreply.github.com>
2025-05-01 19:09:17 +0000

    Updated Iced Cofee

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from quiz_data import quiz, results
 from collections import Counter
+import os
+
+filename = os.path.basename(option["image"])
 
 st.set_page_config(page_title="Which Ece Are You Today?", layout="wide")
 
@@ -20,7 +23,7 @@ if st.session_state.step < len(quiz):
     for i, option in enumerate(q["options"]):
         with cols[i]:
             st.image(option["image"], use_container_width=True)
-            if st.button(f"Pick this", key=f"option_{i}_{st.session_state.step}"):
+            if st.button(filename, key=f"option_{i}_{st.session_state.step}"):
                 st.session_state.answers.append(option["tag"])
                 st.session_state.step += 1
                 st.rerun()
